7a0d09e44fcfa4b54787f8f0694d744f5ed7e624
Nate Pfau <natepfau@yahoo.com>
2021-05-12 20:50:54 -0500

    Add Prompts for all inputs

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,52 @@
-// Assignment code here
+//TODO
+//Arrays of lowercase/upper/numbers/special chars
+//Ask for length between 8-128
+//prompt for each of string arrays
+//concat strings togethers as prompts go on
+//check for no on all prompts and start over
+//loop through length and random on the string
+function generatePassword()
+{
+  var passwordLength = lengthPrompt();
+  var lowerCase = characterPrompt("lowerCase");
+  var upperCase = characterPrompt("upperCase");
+  var numericalCase = characterPrompt("numerical");
+  var specialCase = characterPrompt("special");
+}
 
+function lengthPrompt()
+{
+  var length = window.prompt("How long do you want your password? (Enter # between 8-128)");
+  length = parseInt(length);
+  if(length >= 8 && length <= 128)
+  {
+    return length;
+  }
+  window.alert("Please enter a valid input.")
+  return lengthPrompt();
+}
+
+function characterPrompt(type)
+{
+  var responce = window.prompt("Do you want " + type + " characters? (Yes/No)").toLowerCase();
+
+  switch(responce)
+  {
+    case "yes": 
+    {
+      return true;
+    }
+    case "no":
+    {
+      return false;
+    }
+    default:
+    {
+      window.alert("Please enter a valid input.")
+      return characterPrompt(type);
+    }
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
